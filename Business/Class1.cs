ac3a85e2b31968f51b6ee87d3d8fa15b1ae91487
PaulWilson <Wilsop98@gmail.com>
2018-01-25 17:54:06 +0000

    Adding Files
    All the data from my coursework

---
+++ Business/Class1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class Booking
+    {
+
+        private int totalDays;
+        private int totalGuest;
+        private DateTime startDate;
+        private DateTime endDate;
+        private Boolean breakfast;
+        private Boolean dinner;
+        private Boolean carHire;
+
+
+        public int TotalDays
+        {
+            get { return totalDays; }
+
+            set {
+
+                if (value < 1)
+                {
+                    throw new ArgumentException(" Cannot stay for 0 days");
+                }
+                    totalDays = value; }
+        }
+
+        public int TotalGuest
+        {
+            get { return totalGuest; }
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException(" Cannot have less than one guest");
+                }
+                totalGuest = value;
+            }
+        }
+
+        public DateTime StartDate
+        {
+            get { return startDate; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException("Please insert a start date");
+                }
+                startDate = value; }
+        }
+
+        public DateTime EndDate
+        {
+            get { return endDate; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException("Please insert an end date");
+                }
+                endDate = value; }
+        }
+
+        public Boolean Breakfast
+        {
+            get {return breakfast;}
+            set { breakfast = value;}
+        }
+
+        public Boolean Dinner
+        {
+            get { return dinner; }
+            set { dinner = value; }
+        }
+
+        public Boolean CarHire
+        {
+            get { return carHire; }
+            set { carHire = value; }
+        }
+    }
+}
---
+++ Presentation/NewBooking.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Presentation
+{
+    /// <summary>
+    /// Interaction logic for NewUser.xaml
+    /// </summary>
+    public partial class NewUser : Window
+    {
+        public NewUser()
+        {
+            InitializeComponent();
+        }
+        /*
+         * TO DO
+         * Complete Validation
+         * Recieve All inputs
+         * Complete Total
+         * pat self on back
+         */
+
+
+        private void GuestIn_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            
+            
+        }
+
+        private void BFast_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Cnfrm_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Booking newBooking = new Booking();
+                newBooking.StartDate = StrtDte.SelectedDate.Value;
+                newBooking.EndDate = Enddte.SelectedDate.Value;
+                newBooking.TotalDays = (Convert.ToInt16(newBooking.EndDate - newBooking.StartDate));
+                newBooking.TotalGuest = (Convert.ToInt16(GuestIn));
+                if (BFast.IsChecked == true)
+                {
+                    newBooking.Breakfast = true;
+                }
+                if (Dinner.IsChecked == true)
+                {
+                    newBooking.Dinner = true;
+                }
+                if (Cars.IsChecked == true)
+                {
+                    newBooking.CarHire = true;
+                }
+            }catch (Exception excep)
+ {
+ Console.WriteLine(excep.Message);
+ }
+
+        }
+    }
+}
